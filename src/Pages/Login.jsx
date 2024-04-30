b5da7a2e1c7ff8a7a6907103654457bcd8c8b3f5
DeAvZ801 <rsdqag@inbox.lv>
2024-05-01 00:03:09 +0300

    Login

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from 'react';
+import '../index.css'; 
+import logo from '../Assets/logo_small.png'; 
 
 function Login() {
   return (
-    <div>
-      <h1 style={{ color: 'green' }}>Ez!</h1>
+    <div className="Login">
+      <header className="Login-header">
+        <div className="logo-container">
+          <img src={logo} className="Login-logo" alt="logo" />
+        </div>
+        <form className="login-form">
+          <input type="email" placeholder="E-pasts" required />
+          <input type="password" placeholder="Parole" required />
+          <button type="submit">PIESLĒGTIES</button>
+        </form>
+        <a href="/" className="forgot-password-link">Aizmirsāt paroli?</a>
+      </header>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
